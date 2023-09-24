607d0320150a82c42b8427033f5749e6339c84a3
Matheus Henrique Marques Lourenço <matheuslourenco200@gmail.com>
2023-09-24 17:33:25 -0300

    Adicionado pet e dieta e veterinario

---
+++ backend/HDP.Persistence/Repository/Contracts/IDietaRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace HDP.Persistence.Repository.Contracts;
+
+public class IDietaRepository
+{
+    Task<Dieta[]> GetDietas();
+    Task<Dieta> GetDietaPorId(int idDieta);
+}
---
+++ backend/HDP.Persistence/Repository/Implementations/DietaRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HDP.Persistence.Repository.Implementations;
+
+public class DietaRepository : GeneralRepository
+{
+    private readonly HDPContext _context;
+    public DietaRepository(HDPContext context) : base(context)
+    {
+        _context = context;
+    }
+    
+    public async Task<Dieta[]> GetDietas()
+    {
+        var mainQuery = from dieta in _context.Dieta
+            select dieta;
+        return await mainQuery.ToArrayAsync();    
+    }
+    
+    public async Task<Dieta> GetDietaPorId(int idDieta)
+    {
+        var mainQuery = from dieta in _context.Dieta
+            where dieta.DietaId == idDieta
+            select dieta;
+        
+        return await mainQuery.FirstOrDefaultAsync();   
+    }
+}
