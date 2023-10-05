7da3d24785f0c60a57213811402938686da0c4f4
brennofsr <45960177+brennofsr@users.noreply.github.com>
2023-10-05 19:37:24 -0300

    Criada a página home

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -42,6 +43,7 @@ import {
     AppComponent,
     HospedagemComponent,
     NavbarComponent,
+    HomeComponent,
     CadastrarHospedagemComponent,
     EditarHospedagemComponent,
     DetalhesHospedagemComponent,
